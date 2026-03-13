8359e16b427d744e21702669d8bd67b90b35849d
Andrea Spelgatti <spelgattiandrea@ourouk.be>
2026-03-13 10:46:50 +0100

    fix: allow the use of shell command  following envstub command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+# If first arg starts with < or contains >, it's likely shell syntax
+# so run everything through sh -c
+case "$1" in
+    \<*|*\>*)
+        echo "Executing command: envsubst $*"
+        exec sh -c "envsubst $*"
+        ;;
+esac
+
 # Prepend "envsubst" if the first argument is not an executable
 if ! type -- "$1" &> /dev/null; then
     set -- envsubst "$@"
